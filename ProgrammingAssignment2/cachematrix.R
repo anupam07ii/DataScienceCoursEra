7025f628b0e56c91101b5d926e41925a3d638955
AnupamKumar <anupam07ii@gmail.com>
2017-08-02 21:59:19 +0530

    Update cachematrix.R

--- ProgrammingAssignment2/cachematrix.R
+++ ProgrammingAssignment2/cachematrix.R
@@ -1,3 +1,11 @@
+## The functions defined in this generate the matrix inverse from the calculations
+
+## the makeCacheMatrix function would be returning the list of the following: 
+## 1. matrix values will be set first
+## 2. matrix values will be retreived
+## 3. then we would be able to set the values of inverse matrix
+## 4. finally we would be able to print the values of the inverse matrix
+
 makeCacheMatrix <- function(x = matrix()) {
         inv <- NULL
         set <- function(y) {
@@ -12,6 +20,8 @@ makeCacheMatrix <- function(x = matrix()) {
              setInverse = setInverse,
              getInverse = getInverse)
 }
+
+## Incase the values are not calculated the values of the inverse matrix is calculated else it returns the calculated values
 
 cacheSolve <- function(x, ...) {
         ## Inverse Matrix 'x' is returned
